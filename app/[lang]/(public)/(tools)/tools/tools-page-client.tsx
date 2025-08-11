0b82ca10967c98775892d1cb0cc82b7cfce3dbc4
qiaozh <q24421286632022zh@126.com>
2025-08-11 15:46:47 +0800

    tools

--- app/[lang]/(public)/(tools)/tools/tools-page-client.tsx
+++ app/[lang]/(public)/(tools)/tools/tools-page-client.tsx
@@ -11,67 +11,67 @@ export default function BlogsPageClient() {
 
   const toolCategories = [
     { 
-      id: 1,
+      key: 1,
       slug: 'ai-megapro',
       title: "AIMEGApro", 
       icon: "/tools/logo1.png",
     },
     { 
-      id: 2,
+      key: 2,
       slug: 'ai-productivity-tools',
       title: "AI Productivity Tools", 
       icon: "/tools/logo2.png",
     },
     { 
-      id: 3, 
+      key: 3, 
       slug: 'ai-video-tools',
       title: "AI Video Tools", 
       icon: "/tools/logo3.png",
     },
     { 
-      id: 4, 
+      key: 4, 
       slug: 'ai-text-generators',
       title: "AI Text Generators", 
       icon: "/tools/logo4.png",
     },
     { 
-      id: 5, 
+      key: 5, 
       slug: 'ai-business-tools',
       title: "AI Business Tools", 
       icon: "/tools/logo5.png",
     },
     { 
-      id: 6, 
+      key: 6, 
       slug: 'ai-image-tools',
       title: "AI Image Tools", 
       icon: "/tools/logo6.png",
     },
     { 
-      id: 7, 
+      key: 7, 
       slug: 'automation-tools',
       title: "Automation Tools", 
       icon: "/tools/logo7.png",
     },
     { 
-      id: 8, 
+      key: 8, 
       slug: 'ai-art-generators',
       title: "AI Art Generators", 
       icon: "/tools/logo8.png",
     },
     { 
-      id: 9, 
+      key: 9, 
       slug: 'ai-audio-generators',
       title: "AI Audio Generators", 
       icon: "/tools/logo9.png",
     },
     { 
-      id: 10, 
+      key: 10, 
       slug: 'ai-code-tools',
       title: "AI Code Tools", 
       icon: "/tools/logo10.png",
     },
     { 
-      id: 11, 
+      key: 11, 
       slug: 'misc-ai-tools',
       title: "Misc AI Tools", 
       icon: "/tools/logo11.png",
@@ -95,9 +95,9 @@ export default function BlogsPageClient() {
             // <Link href={`/${params.lang}/blogs/${tool.slug}`} passHref>
             <Link href={`/tools/${tool.slug}`}>
               <div 
-                key={tool.id}
-                style={{display: 'flex', alignItems: 'center', justifyContent: 'center', marginRight: (tool.id == 4 || tool.id == 8) ? '0' : '40px', marginBottom: '20px', borderRadius: '8px', boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.08)',boxSizing: 'border-box', border: '1px solid', borderImage: hoveredCard === tool.id ? 'linear-gradient(180deg, #9776F5 0%, #9776F5 100%) 1.02' : 'linear-gradient(180deg, #BFDEE4 0%, #C0D6DB 100%) 1', background: hoveredCard === tool.id ? 'linear-gradient(154deg, #F6F5FF 13%, #FFFFFF 92%)' : 'linear-gradient(154deg, #F5FBFF 13%, #FFFFFF 92%)', width: '290px', height: '139px', cursor: 'pointer'}}
-                onMouseEnter={() => setHoveredCard(tool.id)}
+                key={tool.key}
+                style={{display: 'flex', alignItems: 'center', justifyContent: 'center', marginRight: (tool.key == 4 || tool.key == 8) ? '0' : '40px', marginBottom: '20px', borderRadius: '8px', boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.08)',boxSizing: 'border-box', border: '1px solid', borderImage: hoveredCard === tool.key ? 'linear-gradient(180deg, #9776F5 0%, #9776F5 100%) 1.02' : 'linear-gradient(180deg, #BFDEE4 0%, #C0D6DB 100%) 1', background: hoveredCard === tool.key ? 'linear-gradient(154deg, #F6F5FF 13%, #FFFFFF 92%)' : 'linear-gradient(154deg, #F5FBFF 13%, #FFFFFF 92%)', width: '290px', height: '139px', cursor: 'pointer'}}
+                onMouseEnter={() => setHoveredCard(tool.key)}
                 onMouseLeave={() => setHoveredCard(null)}
               >
                 {/* 基础卡片内容 */}
@@ -108,7 +108,7 @@ export default function BlogsPageClient() {
                       alt={tool.title} 
                     />
                   </div>
-                  <div style={{color: hoveredCard === tool.id ? '#9776F5' : '#515C6B'}}>{tool.title}</div>
+                  <div style={{color: hoveredCard === tool.key ? '#9776F5' : '#515C6B'}}>{tool.title}</div>
                 </div>
               </div>
             </Link>
