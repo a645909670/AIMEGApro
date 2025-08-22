bcddbf112d4ee14c3a91abebf11252326e347556
qiaozh <q24421286632022zh@126.com>
2025-08-22 15:14:08 +0800

    tools

--- app/[lang]/(public)/(tools)/tools/[slug]/BlogPostList.tsx
+++ app/[lang]/(public)/(tools)/tools/[slug]/BlogPostList.tsx
@@ -18,6 +18,9 @@ interface BlogListItem {
   details1: any;
   details2: any;
   details3: any;
+  details4: any;
+  details5: any;
+  details6: any;
 }
 
 interface BlogPost {
@@ -129,6 +132,7 @@ export default function BlogPostList({ post, params }: BlogPostListProps) {
                   <div
                     key={item.key}
                     style={{
+                      marginBottom: '16px',
                       borderRadius: '8px',
                       boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.08)',
                       boxSizing: 'border-box',
@@ -159,9 +163,9 @@ export default function BlogPostList({ post, params }: BlogPostListProps) {
             <div>
               <div style={{padding: '12px 0', cursor: 'pointer', color: 'rgb(107 114 128)'}} className="w-full max-w-7xl mx-auto rounded-2xl" onClick={() => handleItemClick(true, null)}>&lt;&lt; Return classification List</div>
               <div style={{background: 'linear-gradient(180deg, #F3F3FF 0%, rgba(243, 243, 243, 0) 100%)'}}>
-                <div style={{display: 'flex', paddingTop: '90px'}} className="w-full max-w-7xl mx-auto rounded-2xl mb-8 text-stone-800 dark:text-white">
-                  <div style={{marginRight: '44px', fontSize: '0px'}}>
-                    <img src="/tools/bean-buns/logo.png" alt="" />
+                <div style={{display: 'flex', paddingTop: '90px', paddingLeft: '80px'}} className="w-full max-w-7xl mx-auto rounded-2xl mb-8 text-stone-800 dark:text-white">
+                  <div style={{marginRight: '80px', fontSize: '0px'}}>
+                    <img src={selectedItem?.img} alt="" />
                   </div>
                   <div style={{marginTop: '4px'}}>
                     <div style={{fontSize: '40px', fontWeight: '600'}}>{selectedItem?.title}</div>
@@ -169,7 +173,7 @@ export default function BlogPostList({ post, params }: BlogPostListProps) {
                     <div style={{marginBottom: '48px', fontSize: '16px', color: '#B1B3B8'}}>{selectedItem?.classification}</div>
                     <a href={selectedItem?.address} target="_blank">
                       <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '8px 0', borderRadius: '4px', width: '230px', fontSize: '20px', background: '#5961F9', color: '#fff', cursor: 'pointer'}}>
-                        <div style={{marginRight: '12px'}}>访问官网</div>
+                        <div style={{marginRight: '12px'}}>Visit Site</div>
                         <img src="/tools/lj.png" alt="" />
                       </div>
                     </a>
@@ -188,7 +192,6 @@ export default function BlogPostList({ post, params }: BlogPostListProps) {
                   <div style={{marginBottom: '40px'}}>
                     {selectedItem?.details1.describe}
                   </div>
-                  <div><img src={selectedItem?.details1.img} alt="" /></div>
                 </div>
 
                 <div style={{marginTop: '40px'}}>
@@ -208,6 +211,7 @@ export default function BlogPostList({ post, params }: BlogPostListProps) {
                   </ul>
                 </div>
 
+                
                 <div style={{marginTop: '40px'}}>
                   <div style={{display: 'flex', alignItems: 'center', marginBottom: '24px', fontSize: '28px', fontWeight: '600'}}><div style={{marginRight: '16px', height: '20px', width: '4px', backgroundColor: '#9776F5'}}></div>
                     {selectedItem?.details3.title}
@@ -215,6 +219,59 @@ export default function BlogPostList({ post, params }: BlogPostListProps) {
 
                   <ul>
                     {selectedItem?.details3.introduce.map((item: any, index: any) => (
+                      <li key={index} style={{display: 'flex', marginBottom: '24px'}}>
+                        <span style={{marginRight: '8px'}}>•</span>
+                        <div>
+                          {item}
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+
+                
+                <div style={{marginTop: '40px'}}>
+                  <div style={{display: 'flex', alignItems: 'center', marginBottom: '24px', fontSize: '28px', fontWeight: '600'}}><div style={{marginRight: '16px', height: '20px', width: '4px', backgroundColor: '#9776F5'}}></div>
+                    {selectedItem?.details4.title}
+                  </div>
+
+                  <ul>
+                    {selectedItem?.details4.introduce.map((item: any, index: any) => (
+                      <li key={index} style={{display: 'flex', marginBottom: '24px'}}>
+                        <span style={{marginRight: '8px'}}>•</span>
+                        <div>
+                          {item}
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+
+                
+                <div style={{marginTop: '40px'}}>
+                  <div style={{display: 'flex', alignItems: 'center', marginBottom: '24px', fontSize: '28px', fontWeight: '600'}}><div style={{marginRight: '16px', height: '20px', width: '4px', backgroundColor: '#9776F5'}}></div>
+                    {selectedItem?.details5.title}
+                  </div>
+
+                  <ul>
+                    {selectedItem?.details5.introduce.map((item: any, index: any) => (
+                      <li key={index} style={{display: 'flex', marginBottom: '24px'}}>
+                        <span style={{marginRight: '8px'}}>•</span>
+                        <div>
+                          {item}
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+
+                {/* <div style={{marginTop: '40px'}}>
+                  <div style={{display: 'flex', alignItems: 'center', marginBottom: '24px', fontSize: '28px', fontWeight: '600'}}><div style={{marginRight: '16px', height: '20px', width: '4px', backgroundColor: '#9776F5'}}></div>
+                    {selectedItem?.details6.title}
+                  </div>
+
+                  <ul>
+                    {selectedItem?.details6.introduce.map((item: any, index: any) => (
                     <li key={index} style={{display: 'flex', marginBottom: '4px', cursor: 'pointer'}}>
                       <span style={{marginRight: '8px'}}>•</span>
                       <div>
@@ -223,7 +280,7 @@ export default function BlogPostList({ post, params }: BlogPostListProps) {
                     </li>
                     ))}
                   </ul>
-                </div>
+                </div> */}
               </div>
             </div>
           <div>
